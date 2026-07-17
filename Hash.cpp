e679ba81f73c0a6ff8030286e8e38573f9559bc2
IbnuParvez <parvezfatma98@gmail.com>
2026-07-17 12:15:34 +0300

    Removed hashing files to avoid complexities

--- Hash.cpp
+++
@@ -1,8 +0,0 @@
-#include "Hash.h"
-#include <functional>
-
-std::string hashPassword(const std::string& password) {
-    std::hash<std::string> hasher;
-    size_t hashed = hasher(password);
-    return std::to_string(hashed);
-}
